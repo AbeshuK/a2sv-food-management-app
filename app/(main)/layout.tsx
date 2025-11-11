1778e1bc3c02c234f709159c9d01bf0917632ad5
AbeshuK <abeshuk@awashbank.com>
2025-11-11 14:44:58 +0300

    deployment url changed

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ interface AppLayoutProps {
 }
 
 export const metadata: Metadata = {
-    metadataBase: new URL('https://a2sv-food-app.vercel.app/pages/restaurants'), 
+    metadataBase: new URL('https://a2sv-food-management.vercel.app/pages/restaurants'), 
     title: 'A2SV Food Management App',
     description: 'Manage and explore featured foods and restaurants',
     robots: { index: false, follow: false },
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
     openGraph: {
         type: 'website',
         title: 'A2SV Food Management App',
-        url: 'https://a2sv-food-app.vercel.app/pages/restaurants',
+        url: 'https://a2sv-food-management.vercel.app/pages/restaurants',
         description: 'Manage and explore featured foods and restaurants',
         images: ['https://www.primefaces.org/static/social/sakai-react.png'],
         ttl: 604800,
